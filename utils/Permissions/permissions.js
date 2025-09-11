c54a7aef0cee8df6bc7ebafc6fa69469e5db6ae4
Fazil <fazilmm860@gmail.com>
2025-09-11 13:04:22 +0530

    Updated in ManageFLowchart

--- utils/Permissions/permissions.js
+++ utils/Permissions/permissions.js
@@ -53,12 +53,22 @@ const canManageFlowchart = async (user, clientId) => {
     return { allowed: false, reason: 'Client not found' };
   }
 
-  // Consultant Admin: Can manage if they created the lead or their team is assigned
+ // Consultant admin can manage their clients' flowcharts
   if (user.userType === 'consultant_admin') {
-    const createdBy = client.leadInfo?.createdBy;
-    if (createdBy && user._id && createdBy.toString() === user._id.toString()) {
-      return { allowed: true, reason: 'Consultant admin who created lead' };
-    }
+    // Get all consultant IDs under this admin
+    const consultants = await User.find({ 
+      consultantAdminId: user._id,
+      userType: 'consultant'
+    }).select('_id');
+    const consultantIds = consultants.map(c => c._id);
+    consultantIds.push(user._id);
+
+    return (
+      client.leadInfo.consultantAdminId?.toString() === user._id.toString() ||
+      consultantIds.some(id => client.leadInfo.assignedConsultantId?.toString() === id.toString())
+    );
+  }
+
 
     const consultantsUnderAdmin = await User.find({
       consultantAdminId: user.id || user._id,
